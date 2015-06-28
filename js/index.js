a054fd3ba76bfbd87873596ea47788da22d59c05
Tristan Smith <tristan@shortcord.com>
2015-06-28 00:27:25 -0400

    updates

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
-$( document ).ready(function () {
+function windowResize() {
+    var height = $(window).height();
+    $('#banner').css('height', height );
+}
+
+$(document).ready(function () {
+    windowResize();
     window.setTimeout($("#hi").css("color", "#DDD"), 500);
+    
 });
+
+$(window).resize(function () { windowResize(); });

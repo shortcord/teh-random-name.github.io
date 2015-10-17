01626c147e3d5114e156c208380490984f3fef55
Tristan Smith <tristan@shortcord.com>
2015-10-16 22:48:34 -0400

    things

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
+var quotes = [
+	"We cling to what's familiar and even make the negativity strangely pleasurable.\nBecoming super motivated, in this case, only leads to unfamiliar territory and is intimidating.",
+	"White isn't white it's bright Red Green and Blue.",
+	"That's my secret, I'm always tired."
+];
+
+function quoteGen() {
+	//var ran = Math.floor((Math.random() * ((quotes.length)) - 0) + 0);
+	var selectedQuote = quotes[2];
+	console.log(selectedQuote);
+	$("#jsQuote").text(selectedQuote);
+}
+
 function windowResize() {
     var height = $(window).height();
     $('#banner').css('height', height );
@@ -6,7 +19,7 @@ function windowResize() {
 $(document).ready(function () {
     windowResize();
     window.setTimeout($("#hi").css("color", "#DDD"), 500);
-    
+	quoteGen();
 });
 
 $(window).resize(function () { windowResize(); });

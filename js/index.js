f6fcac5b55ef7f37d863edda15de38bf3c9b3b8b
TristanSmith <tristan@shortcord.com>
2016-01-04 22:21:44 -0500

    some updates
    redid 404 page
    changed main background
    added ran quote thing

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
 var quotes = [
-	"We cling to what's familiar and even make the negativity strangely pleasurable.\nBecoming super motivated, in this case, only leads to unfamiliar territory and is intimidating.",
 	"White isn't white it's bright Red Green and Blue.",
-	"That's my secret, I'm always tired."
+	"That's my secret, I'm always tired.",
+	"while (true) { Console.WriteLine(\"This is the song that never ends\"); }",
+	"I do shit sometimes or something",
+	"megic",
+	"orginal shit"
 ];
 
 function quoteGen() {
-	//var ran = Math.floor((Math.random() * ((quotes.length)) - 0) + 0);
-	var selectedQuote = quotes[2];
+	var ran = Math.floor((Math.random() * ((quotes.length)) - 0) + 0);
+	var selectedQuote = quotes[ran];
 	console.log(selectedQuote);
 	$("#jsQuote").text(selectedQuote);
 }
@@ -18,8 +21,10 @@ function windowResize() {
 
 $(document).ready(function () {
     windowResize();
-    window.setTimeout($("#hi").css("color", "#DDD"), 500);
-	quoteGen();
+    window.setTimeout(function() {$("#hi").css("color", "#DDD")}, 500);
+	if ($("html").attr("id") === "index") {
+		quoteGen();		
+	}
 });
 
 $(window).resize(function () { windowResize(); });

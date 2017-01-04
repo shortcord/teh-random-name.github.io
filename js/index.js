33bac70dd8a800b3763171a98159f1b5b2cea428
Tristan Smith <tristan@shortcord.com>
2017-01-04 07:29:55 -0500

    back to space

--- js/index.js
+++ js/index.js
@@ -1,30 +1,10 @@
-var quotes = [
-	"White isn't white it's bright Red Green and Blue.",
-	"That's my secret, I'm always tired.",
-	"while (true) { Console.WriteLine(\"This is the song that never ends\"); }",
-	"I do shit sometimes or something",
-	"megic",
-	"orginal shit"
-];
+OnResize();
+$(document).ready(function () {
+    $(window).resize(OnResize);
+    window.setTimeout(function() {$("#hi").css("color", "#DDD")}, 500);
+});
 
-function quoteGen() {
-	var ran = Math.floor((Math.random() * ((quotes.length)) - 0) + 0);
-	var selectedQuote = quotes[ran];
-	console.log(selectedQuote);
-	$("#jsQuote").text(selectedQuote);
-}
-
-function windowResize() {
+function OnResize() {
     var height = $(window).height();
     $('#banner').css('height', height );
 }
-
-$(document).ready(function () {
-    windowResize();
-    window.setTimeout(function() {$("#hi").css("color", "#DDD")}, 500);
-	if ($("html").attr("id") === "index") {
-		quoteGen();		
-	}
-});
-
-$(window).resize(function () { windowResize(); });

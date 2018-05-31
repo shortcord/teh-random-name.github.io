e7148f75935475ab6693f4c6c3d6f02983c90dfe
Tristan Smith <tristan@shortcord.com>
2018-05-31 10:36:22 -0400

    Update index.js
    lol forgot to remove particles

--- js/index.js
+++ js/index.js
@@ -3,6 +3,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     window.setTimeout(function() {
         document.getElementById('hi').style.color = '#DDD';
-        particlesJS("background", jsonObject);
     }, 500);
 }, false);
